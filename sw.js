ecfd55c3d5abe08a7e53ae125068ab23a780fcba
Lenin Zapata <lenin.zapata.1993@gmail.com>
2025-12-29 23:30:25 -0500

    chore: update zoneId in service worker configuration

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "3nbf4.com",
-    "zoneId": 10393194
+    "zoneId": 10393488
 }
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
